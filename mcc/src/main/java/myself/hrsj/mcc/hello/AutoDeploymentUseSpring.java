20fc5f847e68480043778d713c513c7981cb8a3c
Harry <694470571@qq.com>
2017-07-02 11:05:55 +0800

    改用MySQL数据库

--- mcc/src/main/java/myself/hrsj/mcc/hello/AutoDeploymentUseSpring.java
+++ mcc/src/main/java/myself/hrsj/mcc/hello/AutoDeploymentUseSpring.java
@@ -1,6 +1,7 @@
 package myself.hrsj.mcc.hello;
 
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
@@ -20,6 +23,11 @@ public class AutoDeploymentUseSpring {
     public void testAutoDeployment() {
         long count = repositoryService.createProcessDefinitionQuery().count();
         assertEquals(1, count);
+        
+        List<ProcessDefinition> list = repositoryService.createProcessDefinitionQuery().list();
+        for (ProcessDefinition processDefinition : list) {
+            repositoryService.deleteDeployment(processDefinition.getDeploymentId(), true);
+        }
     }
 
 }
